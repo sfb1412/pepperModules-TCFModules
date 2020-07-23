1f271e122f71ea0f627fcc4049701e67afe1b7af
felixgolcher <felix.golcher@hu-berlin.de>
2020-07-23 14:04:52 +0200

    introduction of new property ignoreFullText

--- src/main/java/org/corpus_tools/peppermodules/tcfModules/TCFImporterProperties.java
+++ src/main/java/org/corpus_tools/peppermodules/tcfModules/TCFImporterProperties.java
@@ -33,14 +33,31 @@ public class TCFImporterProperties extends PepperModuleProperties {
 	 **/
 	public static final String PROP_USE_COMMON_ANNOTATED_ELEMENT = "useCommonAnnotatedElement";
 
+	/**
+	 * if this property is "true" the full text is ignored.
+	 **/
+	public static final String PROP_IGNORE_FULL_TEXT = "ignoreFullText";
+
 	public TCFImporterProperties() {
 		addProperty(new PepperModuleProperty<Boolean>(PROP_SHRINK_TOKEN_ANNOTATIONS, Boolean.class, "property that sais if spans are always used in tagging of tokens or only if necessary. Value of String has to be \"true\" or \"false\".", true, true));
 		addProperty(new PepperModuleProperty<Boolean>(PROP_USE_COMMON_ANNOTATED_ELEMENT, Boolean.class, "if this property is \"true\" spans are reused by TCFMapperImport and all annotations are stored at a common span. In case \"false\" on each level tcf annotation level a new span is created. Value of String has to be \"true\" or \"false\".", false, true));
+		addProperty(new PepperModuleProperty<Boolean>(PROP_IGNORE_FULL_TEXT, Boolean.class, "if this property is \"true\", full text is ignored.", false, true));
 	}
 
 	public boolean isShrinkTokenAnnotation() {
 		boolean retVal = false;
 		String prop = getProperty(PROP_SHRINK_TOKEN_ANNOTATIONS).getValue().toString();
+		if ((prop != null) && (!prop.isEmpty())) {
+			retVal = Boolean.valueOf(prop);
+		}
+		return retVal;
+	}
+	/*
+	added by analogy
+	 */
+	public boolean isIgnoreFullText() {
+		boolean retVal = false;
+		String prop = getProperty(PROP_IGNORE_FULL_TEXT).getValue().toString();
 		if ((prop != null) && (!prop.isEmpty())) {
 			retVal = Boolean.valueOf(prop);
 		}
